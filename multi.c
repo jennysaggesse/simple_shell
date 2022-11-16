97f8e9540b5f4236f32ba1a818e7a8aa794ebc13
jennysaggesse <uchendujennifer744@gmail.com>
2022-11-16 11:17:24 -0800

    Simple code

---
+++ multi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - This is a shell paired project
+ *Description: this code prints a simple sentence
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	printf("Betty would be proud\n");
+
+}
